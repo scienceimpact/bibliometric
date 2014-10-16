d4faa4e89e1a48d2776ed86013bdb501cada146b
Gregor von Laszewski <laszewski@gmail.com>
2014-10-16 13:59:43 -0400

    adding a dblp example

---
+++ dblp_example.py
@@ -0,0 +1,14 @@
+# http://www.informatik.uni-trier.de/~ley/db/
+
+import dblp
+
+authors = dblp.search('Gregor von Laszewski')
+
+for author in authors:
+    print author.name
+
+authors = dblp.search('Laszewski')
+
+for author in authors:
+    print author.name
+    
