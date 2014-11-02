7269a31390ae33e6d269e7465aacccdd258731ee
siddhardha-m <siddhardha.m@outlook.com>
2014-11-02 17:08:36 -0500

    adding crossref doi lookup interface

--- doi_example.py
+++ doi_example.py
@@ -1,6 +1,7 @@
 from scienceimpact.lookup.doi import doi as doi_crossref
 from scienceimpact.lookup.ieee import doi as doi_ieee
 from scienceimpact.lookup.orcid import doi as doi_orcid
+from scienceimpact.lookup.crossref import doi as doi_crossref_site
 from pprint import pprint
 import json
 
@@ -36,3 +37,6 @@ print doi_orcid("10.1109/GCE.2010.5676126", kind="xml")
 
 banner("ieee orcid")
 print json.dumps(doi_orcid("10.1109/GCE.2010.5676126", kind="json"), indent=2)
+
+banner("crossref site json")
+pprint( doi_crossref_site("10.1109/GCE.2010.5676126", kind="json"))
