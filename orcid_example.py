021ef6c032d4be47dbeabd0d896c786ff02714f3
siddhardha-m <siddhardha.m@outlook.com>
2014-11-03 00:24:35 -0500

    improved orcid example

--- orcid_example.py
+++ orcid_example.py
@@ -1,4 +1,5 @@
 import orcid
+from pprint import pprint
 
 authors = orcid.search('laszewski')
 
@@ -11,5 +12,14 @@ authors = orcid.search('wang')
 print authors
 
 for author in authors:
-    a = author.family_name, 
+    a = author.family_name, author.given_name
     print a
+
+gregor = orcid.get('0000-0001-9558-179X')
+pprint(gregor)
+print gregor.keywords
+
+alfonso = orcid.get('0000-0001-8855-5569')
+pprint(alfonso)
+print alfonso.keywords
+#print alfonso.publications
