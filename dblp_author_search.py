e66d058af08bd877f04a23a94fb2e357423de784
siddhardha-m <siddhardha.m@outlook.com>
2014-11-03 02:19:15 -0500

    adding dblp author search lookup returning authors and thier publication details including co-author details.

---
+++ dblp_author_search.py
@@ -0,0 +1,20 @@
+import dblp
+import json
+
+authors = dblp.search('Gregor von Laszewski')
+
+#for author in authors:
+#    print author.name
+    
+for author in authors:
+    print 70*"="
+    print author.name
+    print 70*"="
+    for pub in author.publications:
+        print '"Key" : "{}", "Title" : "{}", "ISBN" : "{}", "ee" : "{}", "crossref" : "{}", "month" : "{}", "year" : "{}", "mdate" : "{}", "booktitle" : "{}", '\
+        '"type" : "{}", "sub_type" : "{}", "journal" : "{}", "volume" : "{}", "number" : "{}", "chapter" : "{}", "school" : "{}", "url" : "{}", '\
+        '"authors" : "{}", "editors" : "{}", "citations" : "{}", "publisher" : "{}", "series" : "{}", "pages" : "{}"'.format(
+        pub.key, pub.title, pub.isbn, pub.ee, pub.crossref, pub.month, pub.year, pub.mdate, pub.booktitle,
+        pub.type, pub.sub_type, pub.journal, pub.volume, pub.number, pub.chapter, pub.school, pub.url,
+        pub.authors, pub.editors, pub.citations, pub.publisher, pub.series, pub.pages
+        )
