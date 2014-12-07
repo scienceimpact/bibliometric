a99b5c0c84657cfd876d854d03599bb9e87b6b02
Amritanshu Joshi <amritanshujoshi@gmail.com>
2014-12-07 02:26:54 -0500

    no major changes

--- sample/neo4jTesting.py
+++ sample/neo4jTesting.py
@@ -2,7 +2,6 @@ from py2neo import Graph, Node, Relationship
 
 graph = Graph()
 
-
 a = Node("Author", name="Alice")
 
 b = Node("Author", name="Bob")
@@ -14,4 +13,3 @@ a_knows_b = Relationship(a, "KNOWS", b)
 a_knows_b = Relationship(a, "FRIEND", b)
 
 graph.create(a_knows_b)
-
