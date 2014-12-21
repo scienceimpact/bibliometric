aa7d64a64168a53bb9949c63ce81299cc5a8dea4
siddhardha-m <siddhardha.m@outlook.com>
2014-12-21 11:19:13 -0500

    Adding automated install script for Ubuntu

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+python setupFiles/get-pip.py
+
+pip install virtualenv
+
+virtualenv venv_biblio
+
+source venv_biblio/bin/activate
+
+pip install requests
+
+pip install xmltodict
+
+pip install pymongo
+
+pip install py2neo
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
+sudo service mongod start
+
+setupFiles/neo4j-community-2.1.6/bin/neo4j start
+
